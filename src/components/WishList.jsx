1f0be4318f7283f544cb8d8150b0fbf28b28a639
Varun Dunna <varun.dunna2524@gmail.com>
2024-11-04 00:10:15 +0530

    recoil-basic

--- src/components/WishList.jsx
+++ src/components/WishList.jsx
@@ -1,13 +1,31 @@
 import Navbar from "./Navbar";
 import { wishItemsState } from "../store/wishItemsState";
-import { useRecoilValue } from "recoil";
+import { cartItemsState } from "../store/cartItemsState";
+import { useRecoilState, useRecoilValue } from "recoil";
 export default function WishList() {
   const wishItems = useRecoilValue(wishItemsState);
+  const [cartItems, setCartItems] = useRecoilState(cartItemsState);
   return (
     <div className="wishlist">
       <Navbar />
       <div className="super-card">
-        <div className="card">{wishItems.join(",")}</div>
+        {wishItems.map((element, index) => {
+          return (
+            <div
+              className="card"
+              style={{ display: "flex", flexDirection: "column" }}
+            >
+              <p>{element}</p>
+              <button
+                onClick={() => {
+                  setCartItems([...cartItems, element]);
+                }}
+              >
+                Add to cart
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
